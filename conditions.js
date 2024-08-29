0dc963baf80309e11a1814f7ac5a2399f0897973
ashraful-islam-siam <ashrafullislam.dev1@gmail.com>
2024-08-29 15:16:15 +0600

    More condition

--- conditions.js
+++ conditions.js
@@ -1,48 +1,27 @@
+const salary = 21001;
+const isBCS = true;
+const height = 68;
+const hasCar = false;
+// height = 71;
 
+if (salary > 20000 && height > 66) {
+  console.log("Su----------patro");
+} else {
+  console.log("Onno patro khuji");
+}
 
+if (salary > 25000 || height > 72) {
+  console.log("eso baba kobul");
+} else {
+  console.log("vaag tui mokbul");
+}
 
+//------------COMPLEX CONDITION--------------
 
+if ((salary > 25000 && hasCar == true) || isBCS == true) {
+  console.log("tomar 14 gusti raji");
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if ((salary > 25000 || hasCar == true) && isBCS == true) {
+  console.log("tomar 14 gusti raji");
+}
--- if.js
+++ if.js
@@ -1,1 +1,41 @@
-var name = "Ashraful Islam";
+/**
+ * if you buy food for me
+ *         i will come
+ *
+ * ------------------
+ * if briyani price less than 300 taka
+ *          i will buy
+ *
+ *
+ * ------Structure-------
+ * if(condition) {
+ *
+ * }
+ */
+
+if (13 < 10) {
+  console.log("i have a smaller value");
+}
+
+var briyani = 250;
+
+if (briyani < 300) {
+  console.log("mama give me some briyani");
+}
+
+/**
+ * if(condition) {
+ *      execute code if condition above true
+ * }
+ * else {
+ *      execute code if the condition is false
+ * }
+ */
+
+var weight = 20;
+
+if (weight < 20) {
+  console.log("I will carry it by myself");
+} else {
+  console.log("I will rent a rickshaw");
+}
